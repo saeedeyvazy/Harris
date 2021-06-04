82d2e56fa726f663f4f1c3276f96c65214ee2e5c
saeed eyvazy <saeed.eyvazy@gmail.com>
2021-06-04 19:48:37 +0430

    initial commit

--- optimizer.py
+++ optimizer.py
@@ -20,39 +20,17 @@ def selector(algo,func_details,popSize,Iterasyon):
         x=ga.GA(getattr(functions, function_name),lb,ub,dim,popSize,Iterasyon)
     return x
 
-# Çalışmasını istediğiniz algoritmaları "True" ile belirtiniz.
 HHO=True
 GA=False
 
-# Çalışmasını istediğiniz fonksiyonları "True" ile belirtiniz.
 F1=True
 F2=True
 F3=True
-F4=True
-F5=True
-F6=True
-F7=True
-F8=True
-F9=True
-F10=True
-F11=True
-F12=True
-F13=True
-F14=True
-F15=True
-F16=True
-F17=True
-F18=True
-F19=True
-F20=True
-F21=True
-F22=True
-F23=True
+
 
 algorithm=[HHO,GA]
-func=[F1,F2,F3,F4,F5,F6,F7,F8,F9,F10,F11,F12,F13,F14,F15,F16,F17,F18,F19,F20,F21,F22,F23]
+func=[F1, F2, F3]
         
-# Fonksiyonun tekrar sayısını belirtiniz
 FuncAgain=1
 
 # Genel Parametreler
@@ -77,7 +55,6 @@ for i in range (0, len(algorithm)): # Algoritma dizisindeki değerleri kontrol e
     for j in range (0, len(func)): # Fonksiyon dizisindeki değerleri kontrol eder
         if((algorithm[i]==True) and (func[j]==True)): # Değerlerin "True" olup olmadığını kontrol eder
             for k in range (0,FuncAgain): # Fonksiyonların kaç kez çalışacağını kontrol eder
-
                 func_details=functions.getFunctionDetails(j)
                 x=selector(i,func_details,PopulationSize,Iterations)
                 if(Export==True): # CSV formatında çıktı alınıp alınmayacağını kontrol eder
